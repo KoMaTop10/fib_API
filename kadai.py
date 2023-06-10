36792dc2c2633eb85dd208c2a6e7ef97d7cc459f
KoMaTop10 <2y.komachi10@gmail.com>
2023-06-10 17:54:50 +0900

    土台完成

---
+++ kadai.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+def fibonacci(n:int):
+    
+    if n < 0:
+        return -1
+    elif n == 1 or n == 2:
+        return 1
+    
+    a = 1
+    b = 1
+    
+    for i in range(n-2):
+        a,b =b, a + b
+    return b
+        
+
+    
+
+@app.get("/fib/{n}")
+def index(n:int):
+    result = fibonacci(n)
+    return {"result": result}
+
