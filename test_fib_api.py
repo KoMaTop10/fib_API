32d044ab1ef1d6dd637938d4f9ed2033de82408a
KoMaTop10 <2y.komachi10@gmail.com>
2023-06-17 21:04:01 +0900

    テストケースの追加

--- test_fib_api.py
+++ test_fib_api.py
@@ -6,13 +6,32 @@ client = TestClient(app)
 
 def test_read_main():
     result = fibonacci
-    response = client.get("/fib",params={"n":11})
+    response = client.get("/fib",params={"n":99})
     print(response.json)
     assert response.status_code == 200
+    
+def test_read_main_one():
+    result = fibonacci
+    response = client.get("/fib",params={"n":1})
+    print(response.json)
+    assert response.status_code == 200
+    
+def test_read_main_zero():
+    result = fibonacci
+    response = client.get("/fib",params={"n":0})
+    print(response.json)
+    assert response.status_code == 400
 
 def test_read_main_negative():
     result = fibonacci
     response = client.get("/fib",params={"n":-1})
     print(response.json)
     assert response.status_code == 400
+
+def test_read_main_str():
+    result = fibonacci
+    response = client.get("/fib",params={"n":"test"})
+    print(response.json)
+    assert response.status_code == 422
+
 
