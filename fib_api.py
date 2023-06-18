2d0937a57e62d1f17589077baf9eab90a6a68742
KoMaTop10 <2y.komachi10@gmail.com>
2023-06-18 12:47:43 +0900

    エラー処理の変更

--- fib_api.py
+++ fib_api.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI,HTTPException,Request,Query
+from fastapi import FastAPI,HTTPException,Request
 from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 
+#FastAPIの定義
 app = FastAPI()
 
+
+# n番目のフィボナッチ数を返す関数
 def fibonacci(n:int):
     
     if n <= 0:
@@ -18,24 +22,58 @@ def fibonacci(n:int):
         a,b =b, a + b
     return b
 
+
+# "/fib/?n=n"でn番目のフィボナッチ数を出力する
+
 @app.get("/fib")
-def get_fib(n:int=Query()):
-    if n < 1:
-        raise UnicornException(num=n)
+def get_fib(n:int):
+    if n < 0:
+        raise NegativeException(num=n)
+    if n == 0:
+        raise ZeroException(num=n)
     
     result = fibonacci(n)
     return {"result": result}
 
-class UnicornException(Exception):
+
+# nが負の整数の時の例外処理
+
+class NegativeException(Exception):
     def __init__(self, num: int):
         self.num = num
+        
 
-@app.exception_handler(UnicornException)
-async def unicorn_exception_handler(request: Request, exc: UnicornException):
+@app.exception_handler(NegativeException)
+async def negative_exception_handler(request: Request, exc: NegativeException):
     return JSONResponse(
         status_code=400,
         content={
             "status":400,
-            "message":"Bad request"
+            "message":"Negative Number Error"
             },
+    )
+
+# n = 0の時の例外処理
+
+class ZeroException(Exception):
+    def __init__(self, num: int):
+        self.num = num
+
+@app.exception_handler(ZeroException)
+async def zero_exception_handler(request: Request, exc: ZeroException):
+    return JSONResponse(
+        status_code=400,
+        content={
+            "status":400,
+            "message":"Zero Error"
+            },
+    )
+    
+
+#nの型が異なる場合の例外処理
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=400,
+        content={"status": 400, "message": "Validation Error"},
     )
